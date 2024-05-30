12c50af4c42733da5b28432ccbeffc7b49bde34e
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-05-30 09:20:25 -0700

    Prep for next practice.

--- d3-test/src/App.tsx
+++ d3-test/src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
 // import BarChart from "./components/BarChart";
-import FirstD3 from "./components/FirstD3";
-import SVGPractice from "./components/SVGPractice";
-import Smiley from "./components/Smiley";
+// import FirstD3 from "./components/FirstD3";
+// import SVGPractice from "./components/SVGPractice";
+// import Smiley from "./components/Smiley";
 
 function App() {
   return (
     <>
-      <Smiley />
-      <SVGPractice />
-      <FirstD3 />
+      {/* <Smiley /> */}
+      {/* <SVGPractice /> */}
+      {/* <FirstD3 /> */}
       {/* <BarChart /> */}
     </>
   );
