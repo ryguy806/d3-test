4b37787cde2732cb4e2b10a30b74b15f9ef8bca3
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-06-03 09:31:55 -0700

    Learned how to use the join method.

--- d3-test/src/components/JoinData.tsx
+++ d3-test/src/components/JoinData.tsx
@@ -1,9 +1,17 @@
 import * as d3 from "d3";
 
-const data = [20, 30, 95, 27, 42, 69];
+type DataType = {
+  name: string;
+  height: string | number;
+};
 
 const JoinData = () => {
   const svg = d3.select("svg").attr("width", 500).attr("height", 400);
+  const data: DataType[] = [
+    { name: "house", height: 50 },
+    { name: "trailer", height: 20 },
+    { name: "building", height: 75 },
+  ];
 
   const circles = svg.selectAll("circle").data(data);
   circles
@@ -14,13 +22,25 @@ const JoinData = () => {
     })
     .attr("cy", 70)
     .attr("r", (d) => {
-      return d / 2;
+      return d.height;
     })
     .attr("fill", "yellow");
 
+  d3.select("ul")
+    .selectAll("li")
+    .data(data)
+    .join("li")
+    .text((d) => d.name + ": " + d.height);
+
   return (
     <>
-      <svg />
+      <ul>
+        <li>dummy content</li>
+        <li>dummy content</li>
+        <li>dummy content</li>
+        <li>dummy content</li>
+        <li>dummy content</li>
+      </ul>
     </>
   );
 };
